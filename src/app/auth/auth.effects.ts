bb2a74ed0209a48117c34ec1d571de908ccbc7c2
André Pereira <andrelcop@gmail.com>
2025-03-07 18:18:59 +0100

    add login and logout effect

--- src/app/auth/auth.effects.ts
+++ src/app/auth/auth.effects.ts
@@ -1,15 +1,27 @@
 import { Injectable } from "@angular/core";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { AuthActions } from "./action-types";
+import { tap } from "rxjs/operators";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class AuthEffects {
-    constructor(private actions$: Actions) {
-        actions$.subscribe(action => {
-            console.log('AuthEffects', action);
-            if(action.type === '[Login Page] User Login') {
-                localStorage.setItem('user', JSON.stringify(action["user"]));
-            }
-        });
-    }   
+
+    login$ = createEffect(() => this.actions$.pipe(
+        ofType(AuthActions.login),
+        tap(action => localStorage.setItem('user',
+             JSON.stringify(action.user))
+        )),
+    { dispatch: false });
+
+    logout$ = createEffect(() => this.actions$.pipe(
+        ofType(AuthActions.logout),
+        tap(action => {
+            localStorage.removeItem('user');
+            this.router.navigateByUrl('/login');
+         })),
+        { dispatch: false });
+
+    constructor(private actions$: Actions, private router: Router) { }   
 
 }
