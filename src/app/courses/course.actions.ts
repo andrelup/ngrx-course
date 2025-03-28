65e2c4748af6ae06d1fd145848b845764574d134
André Pereira <andrelcop@gmail.com>
2025-03-28 17:40:42 +0100

    add other action to update the course after edit it

--- src/app/courses/course.actions.ts
+++ src/app/courses/course.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from "@ngrx/store";
 import { Course } from "./model/course";
+import { Update } from "@ngrx/entity";
 
 
 export const loadAllCourses = createAction(
@@ -9,4 +10,9 @@ export const loadAllCourses = createAction(
 export const allCoursesLoaded = createAction(
     "[Load Courses Effect] All courses Loaded",
     props<{courses: Course[]}>()
+);
+
+export const courseUpdated = createAction(
+    "[Edit Course Dialog] Course Updated",  
+    props<{update: Update<Course>}>()
 );
