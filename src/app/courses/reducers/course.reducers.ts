c84c886db6a200266f630b746256ed462184dfc6
André Pereira <andrelcop@gmail.com>
2025-03-28 17:47:49 +0100

    update coursesReducer

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -23,7 +23,9 @@ export const coursesReducer = createReducer(
     on(CourseActions.allCoursesLoaded,
     (state, action)=> adapter.addMany(
         action.courses,
-        {...state, allCoursesLoaded: true}))
+        {...state, allCoursesLoaded: true})),
+    on(CourseActions.courseUpdated,
+    (state, action) => adapter.updateOne(action.update, state)),
 );
 
 export const {selectAll} = adapter.getSelectors();
